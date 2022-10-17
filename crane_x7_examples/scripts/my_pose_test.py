bc84a52c0b40028dd515914a00bc0fe649aacd1f
anhquan18 <quan181219@gmail.com>
2022-10-17 22:46:25 +0900

    add world based on shimotori thesis failed experiment

--- crane_x7_examples/scripts/my_pose_test.py
+++ crane_x7_examples/scripts/my_pose_test.py
@@ -5,7 +5,7 @@ import moveit_commander
 import geometry_msgs.msg
 import rosnode
 import numpy as np
-import time
+from sensor_msgs.msg import PointCloud
 from tf.transformations import quaternion_from_euler
 from visualization_msgs.msg import Marker
 
@@ -28,6 +28,7 @@ def rotate_3Dmatrix_z_axis(position_vec, theta):
                                 [0, 0, 1]])
     return np.dot(rotation_matrix, position_vec)
 
+
 def main():
     rospy.init_node("pose_groupstate_example")
     robot = moveit_commander.RobotCommander()
@@ -36,6 +37,11 @@ def main():
     gripper = moveit_commander.MoveGroupCommander("gripper")
     finger_pub = rospy.Publisher("finger_pub", Marker, queue_size=5)
 
+    # ハンドと爪先の間の寸法、単位はメートル
+    finger_pose = np.array([[0.0],
+                            [0.0], 
+                            [0.06 + 0.024]])
+
     while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
         rospy.sleep(1.0)
     rospy.sleep(1.0)
@@ -43,6 +49,10 @@ def main():
     print("Group names:")
     print(robot.get_group_names())
     
+    arm.set_named_target("home")
+    arm.go()
+    rospy.sleep(1)
+
     print("Current state:")
     print(robot.get_current_state().joint_state)
     
@@ -62,8 +72,8 @@ def main():
     arm.go()
     '''
 
-    # ハンドを少し閉じる
-    gripper.set_joint_value_target([0.01, 0.01])
+    # ハンドを閉じる
+    gripper.set_joint_value_target([0.01,0.01])
     gripper.go()
 
     # 手動で姿勢を指定するには以下のように指定
@@ -71,7 +81,7 @@ def main():
     
     target_pose = geometry_msgs.msg.Pose()
     target_pose.position.x = 0.25
-    target_pose.position.y = -0.02
+    target_pose.position.y = -0.03
     target_pose.position.z = 0.336
     q = quaternion_from_euler( 3.14/1, 0.0, 3.14/1 )
     target_pose.orientation.x = q[0]
@@ -81,30 +91,44 @@ def main():
     arm.set_pose_target( target_pose )	# 目標ポーズ設定
     arm.go()							# 実行
 
-    pos_vec = np.array([[target_pose.position.x], 
-                        [target_pose.position.y], 
-                        [target_pose.position.z]])
+    current_pose = arm.get_current_pose().pose
 
-    pos_vec = rotate_3Dmatrix_x_axis(pos_vec, 3.14)
-    pos_vec = rotate_3Dmatrix_y_axis(pos_vec, 0.0)
-    pos_vec = rotate_3Dmatrix_z_axis(pos_vec, 3.14)
+    pos_vec = np.array([[current_pose.position.x], 
+                        [current_pose.position.y], 
+                        [current_pose.position.z]])
+
+    # 指先をハンドと同じ姿勢に回転させる
+    # XYZ軸を順番的に回転する
+    finger_pose = rotate_3Dmatrix_x_axis(finger_pose, 3.14)
+    finger_pose = rotate_3Dmatrix_y_axis(finger_pose, 0.0)
+    finger_pose = rotate_3Dmatrix_z_axis(finger_pose, 3.14)
+
+    # ハンドと同じ姿勢になったハンドを平行移動させる
+    finger_pose += pos_vec
+
 
     while not rospy.is_shutdown():
         finger_marker = Marker()
         finger_marker.header.frame_id = "base_link"
-        finger_marker.header.stamp = rospy.Time.now() 
-
-        finger_marker.ns = "finger_marker"
         finger_marker.id = 1000
+        finger_marker.ns = "finger_marker"
         finger_marker.type = Marker.CUBE
         finger_marker.action = Marker.ADD
+        finger_marker.header.stamp = rospy.Time.now() 
 
         print("x:", pos_vec[0][0])
         print("y:", pos_vec[1][0])
         print("z:", pos_vec[2][0])
-        finger_marker.pose.position.x = target_pose.position.x
-        finger_marker.pose.position.y = target_pose.position.y
-        finger_marker.pose.position.z = target_pose.position.z - 0.06 - 0.024
+        #finger_marker.pose.position.x = target_pose.position.x
+        #finger_marker.pose.position.y = target_pose.position.y
+        #finger_marker.pose.position.z = target_pose.position.z - 0.06 - 0.024
+
+        print("arm current pose:")
+        print(arm.get_current_pose().pose.position)
+
+        finger_marker.pose.position.x = finger_pose[0][0]
+        finger_marker.pose.position.y = finger_pose[1][0]
+        finger_marker.pose.position.z = finger_pose[2][0]
 
         finger_marker.pose.orientation.x = q[0]
         finger_marker.pose.orientation.y = q[1]
@@ -115,14 +139,14 @@ def main():
         finger_marker.color.g = 0.0
         finger_marker.color.b = 0.8
         finger_marker.color.a = 1.0
-        finger_marker.scale.x = 0.02
-        finger_marker.scale.y = 0.06
+        finger_marker.scale.x = 0.01
+        finger_marker.scale.y = 0.05
         finger_marker.scale.z = 0.001
 
-        finger_marker.lifetime = rospy.Duration(0.5)
+        finger_marker.lifetime = rospy.Duration(0.1)
         finger_pub.publish(finger_marker)
 
-        time.sleep(1.0)
+        rospy.sleep(0.1)
 
 
     # 移動後の手先ポーズを表示
